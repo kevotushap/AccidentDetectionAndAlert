62466118b4c13555fa595679f69c753c30ac5988
Kevo tushap <59371076+kevotushap@users.noreply.github.com>
2023-07-26 16:10:58 +0300

    Update AccelerometerService.java

--- app/src/main/java/com/example/accidentdetectionandalert/AccelerometerService.java
+++ app/src/main/java/com/example/accidentdetectionandalert/AccelerometerService.java
@@ -1,23 +1,23 @@
 package com.example.accidentdetectionandalert;
 
-        import android.app.Notification;
-        import android.app.NotificationChannel;
-        import android.app.NotificationManager;
-        import android.app.PendingIntent;
-        import android.app.Service;
-        import android.content.Intent;
-        import android.hardware.Sensor;
-        import android.hardware.SensorEvent;
-        import android.hardware.SensorEventListener;
-        import android.hardware.SensorManager;
-        import android.os.Build;
-        import android.os.IBinder;
-        import android.util.Log;
-        import android.widget.TextView;
-        import android.widget.Toast;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Build;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
-        import androidx.annotation.Nullable;
-        import androidx.core.app.NotificationCompat;
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 
 public class AccelerometerService extends Service implements SensorEventListener {
 
@@ -31,6 +31,13 @@ public class AccelerometerService extends Service implements SensorEventListener
     private static final int MIN_TIME_BETWEEN_SHAKES_MILLISECS = 1000;
     private long mLastShakeTime;
     private TextView counter;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mySensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
