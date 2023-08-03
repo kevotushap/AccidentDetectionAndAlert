38c3d0c4011b3990d520ec26fd2ae1c2ccc1e73d
Kevo tushap <59371076+kevotushap@users.noreply.github.com>
2023-08-03 13:10:59 +0300

    Update AccelerometerService.java

--- app/src/main/java/com/example/accidentdetectionandalert/AccelerometerService.java
+++ app/src/main/java/com/example/accidentdetectionandalert/AccelerometerService.java
@@ -28,7 +28,7 @@ public class AccelerometerService extends Service implements SensorEventListener
     private Sensor accelerometerSensor;
     private float x1, x2, x3, acceleration;
     private static final float ERROR = (float) 7.0;
-    private static final float SHAKE_THRESHOLD = 6.00f; // m/S**2
+    private static final float SHAKE_THRESHOLD = 4.00f; // m/S**2
     private static final int MIN_TIME_BETWEEN_SHAKES_MILLISECS = 1000;
     private long mLastShakeTime;
     private TextView counter;
