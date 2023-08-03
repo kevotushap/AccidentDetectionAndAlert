d818985bbfb1018bebaf5c3d3a1bd1724bb0d375
Kevo tushap <59371076+kevotushap@users.noreply.github.com>
2023-08-03 12:00:36 +0300

    Update LocationFinder.java

--- app/src/main/java/com/example/accidentdetectionandalert/LocationFinder.java
+++ app/src/main/java/com/example/accidentdetectionandalert/LocationFinder.java
@@ -1,19 +1,19 @@
 package com.example.accidentdetectionandalert;
 
-        import android.annotation.SuppressLint;
-        import android.app.AlertDialog;
-        import android.app.Service;
-        import android.content.Context;
-        import android.content.DialogInterface;
-        import android.content.Intent;
-        import android.location.Location;
-        import android.location.LocationListener;
-        import android.location.LocationManager;
-        import android.os.Bundle;
-        import android.os.IBinder;
-        import android.provider.Settings;
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.Service;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.provider.Settings;
 
-        import androidx.annotation.Nullable;
+import androidx.annotation.Nullable;
 
 public class LocationFinder extends Service implements LocationListener {
 
