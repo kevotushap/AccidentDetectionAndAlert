517d98ae8ec088a80eb712889600f8cf53c9a6a8
Kevo tushap <59371076+kevotushap@users.noreply.github.com>
2023-08-05 09:09:17 +0300

    Update Abort.java

--- app/src/main/java/com/example/accidentdetectionandalert/Abort.java
+++ app/src/main/java/com/example/accidentdetectionandalert/Abort.java
@@ -1,22 +1,27 @@
 package com.example.accidentdetectionandalert;
 
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
 
 public class abort extends AppCompatActivity {
 
@@ -29,8 +34,56 @@ public class abort extends AppCompatActivity {
     Button abortBt;
     LocationFinder finder;
 
+    private static final int PERMISSION_REQUEST_CODE = 1;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.abort);
+
+        abortBt = findViewById(R.id.abort1);
+
+        // Check and request SMS permission if not granted
+        if (checkSMSPermission()) {
+            // Permission already granted, proceed with the logic
+            init();
+        } else {
+            // Request permission
+            requestSMSPermission();
+        }
+    }
+
+    // Check if SMS permission is granted
+    private boolean checkSMSPermission() {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS)
+                == PackageManager.PERMISSION_GRANTED;
+    }
+
+    // Request SMS permission
+    private void requestSMSPermission() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.SEND_SMS},
+                PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, proceed with the logic
+                init();
+            } else {
+                // Permission denied, show a message or handle accordingly
+                Toast.makeText(this, "Permission denied. Cannot send SMS.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void init() {
+        // Extract phone numbers
         super.onCreate(savedInstanceState);
         setContentView(R.layout.abort);
         abortBt = findViewById(R.id.abort1);
@@ -64,7 +117,7 @@ public class abort extends AppCompatActivity {
                     ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(150);
                 }
             }
-        }, 10000);
+        }, 2000);
 
         finder = new LocationFinder(this);
         if (finder.canGetLocation()) {
@@ -72,13 +125,13 @@ public class abort extends AppCompatActivity {
             longitude = finder.getLongitude();
         } else {
             finder.showSettingsAlert();
-        }
+         }
 
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 if (!abortButtonClicked && !messageSent) {
-                    sendMessages();
+                    SEND_SMS();
                 }
             }
         }, 15000);
@@ -87,12 +140,12 @@ public class abort extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 abortButtonClicked = true;
-                System.exit(1);
+                finish();
             }
         });
     }
 
-    private void sendMessages() {
+    private void SEND_SMS() {
         Toast.makeText(abort.this, "Latitude" + latitude + " Longitude" + longitude, Toast.LENGTH_LONG).show();
         SmsManager sm = SmsManager.getDefault();
         sm.sendTextMessage(MainActivity.no1, null, "Help! I've met with an accident at http://maps.google.com/?q=" + String.valueOf(latitude) + "," + String.valueOf(longitude) + "\nMy Blood Group is = " + MainActivity.bgrp, null, null);
